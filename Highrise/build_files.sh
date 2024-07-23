45eb16dcb8353a891a8b5234bef2e3194e9707b8
Anuj Choubey <114551422+Anujc07@users.noreply.github.com>
2024-07-23 14:55:11 +0530

    Update build_files.sh

--- Highrise/build_files.sh
+++ Highrise/build_files.sh
@@ -9,14 +9,14 @@ apt-get update && apt-get install -y default-libmysqlclient-dev
 export MYSQLCLIENT_CFLAGS=$(mysql_config --cflags)
 export MYSQLCLIENT_LDFLAGS=$(mysql_config --libs)
 
-# Install Python dependencies
+# Upgrade pip and install Python dependencies
 python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 
 # Create the output directory for static files
-mkdir -p staticfiles_build/static
+mkdir -p staticfiles/static
 
 # Collect static files
-python3.9 Highrise/manage.py collectstatic --noinput --clear --settings=Highrise.settings
+python3.9 manage.py collectstatic --noinput --clear --settings=Highrise.settings
 
 echo "BUILD END"
